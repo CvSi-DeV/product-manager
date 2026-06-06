0d192f84d60609dc0b7468d758f945bcb9e498e7
CVSI <contact@CVSI.nc>
2026-06-06 19:10:31 +1100

    feature: connection to express server

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { appContainer, h2Title, productCountSpan, productGrill } from './styles/theme';
 import { type ProductFilterStock, type ProductSortBy, type Product } from './types/Product';
-import { INITIAL_PRODUCTS } from './data/initialDatas';
 import ProductCard from './components/ProductCard';
 import Cart from './components/Cart';
 import Statistics from './components/Statistics';
 import SearchBar from './components/SearchBar';
 import FilterPanel from './components/FilterPanel';
 import ProductForm from './components/ProductForm';
+import { API_URL, PRODUCT_URL } from './config/api';
 
 function App() {
   //STATES
   //Products
-  const [products, setProducts] = useState<Product[]>(INITIAL_PRODUCTS);
+  const [products, setProducts] = useState<Product[]>([]);
 
   //Cart 
   const [productsInCart, setProductsInCart] = useState<number[]>([]);
@@ -24,16 +24,76 @@ function App() {
   const [filterStock, setFilterStock] = useState<ProductFilterStock>("all");
   const [sortBy, setSortBy] = useState<ProductSortBy>('name');
 
-  //ProductForm
-  const handleAddProduct = (product: Omit<Product, 'id'>) => {
-    //Déterminer un nouvel Id
-    const newId = Math.max(...products.map(p => p.id), 0) + 1;
+  //Indicateur de chargement de données
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
 
-    //Créer un nouveau produit
-    const newProduct = { ...product, id: newId };
+  useEffect(() => {
+    const loadProduct = async () => {
+      try {
+        setIsLoading(true);
+        //appel fetch 
+        const response = await fetch(`${API_URL}${PRODUCT_URL}`);
 
+        if (!response.ok) throw new Error(`Error ${response.status}`);
+
+        const data = await response.json();
+        if (data === undefined) throw new Error(`Aucun produit à charger`);
+        setProducts(data);
+
+      } catch {
+        setError('Impossible de charger les produits');
+
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    loadProduct();
+  }, []);
+
+  //Ajouter un produit
+  const handleAddProduct = async (product: Omit<Product, 'id'>) => {
+    const response = await fetch(`${API_URL}${PRODUCT_URL}`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(product)
+    });
+
+    if (!response.ok) throw new Error(`Ajout du nouveau produit ${product.name} impossible. ` + `Code Erreur : ${response.status}`);
     //Ajouter le nouveau produit à la liste
-    setProducts([...products, newProduct]);
+    const stageProduct = await response.json();
+    setProducts([...products, stageProduct])
+  };
+
+  //Supprimer un produit
+  const handleDelete = async (id: number) => {
+    if (!products.find(p => p.id === id)) throw new Error(`Produit inexistant ${id} : impossible de supprimer le produit`);
+
+    const response = await fetch(`${API_URL}${PRODUCT_URL}/${id}`,
+      { method: 'DELETE' }
+    );
+
+    if (!response.ok) throw new Error(`Error ${response.status}`)
+
+    setProducts(products.filter(p => p.id !== id));
+  };
+
+  const handleUpdateStock = async (id: number, newStock: number) => {
+    if (!products.find(p => p.id === id)) throw new Error(`Produit inexistant ${id} : impossible de mettre à jour le stock`);
+
+    const response = await fetch(`${API_URL}${PRODUCT_URL}/${id}`,
+      {
+        method: 'PATCH',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ stock: newStock })
+      });
+
+    if (!response.ok) throw new Error(`Error ${response.status}`);
+
+    setProducts(products.map(p =>
+      p.id === id ? { ...p, stock: newStock } : p
+    ));
   };
 
   const handleAddToCart = (id: number) => {
@@ -46,16 +106,6 @@ function App() {
     console.log("panier: ", productsInCart);
   };
 
-  const handleDelete = (id: number) => {
-    setProducts(products.filter(p => p.id !== id));
-  };
-
-  const handleUpdateStock = (id: number, newStock: number) => {
-    setProducts(products.map(p =>
-      p.id === id ? { ...p, stock: newStock } : p
-    ));
-  };
-
   // Rechercher par le nom 
   const searchedProducts = products.filter((product) => (product.name.toLowerCase().includes(searchedTerm.toLowerCase())));
 
@@ -66,7 +116,8 @@ function App() {
     return true;  // 'all'
   });
 
-  // tri par le critère
+
+  // Trier par le critère
   const sortedProducts = [...filteredProducts].sort((p1, p2) => {
     if (sortBy === 'name')
       return p1.name.localeCompare(p2.name);
@@ -79,6 +130,27 @@ function App() {
 
     return 0;
   });
+
+  if (isLoading) return (
+    <div style={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      minHeight: '100vh'
+    }}>
+      ⏳ Chargement des produits...
+    </div>
+  );
+
+  if (error) return (
+    <div style={{
+      color: '#ef4444',
+      padding: '40px',
+      textAlign: 'center'
+    }}>
+      ❌ {error}
+    </div>
+  );
 
   return (
     <div style={appContainer}>
