f3ddbc1eaf3593e7f02c374282222f5b695e8972
CVSI <contact@CVSI.nc>
2026-06-12 15:15:59 +1100

    feat: VITE_API_URL environment variable

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,4 +1,5 @@
-export const API_URL = 'http://localhost:3000';
+//export const API_URL = 'http://localhost:3000';
+export const API_URL = import.meta.env.VITE_API_URL ?? 'http://localhost:3000';
 export const PRODUCT_URL = '/api/products';
 export const AUTH_URL = '/api/auth';
 
